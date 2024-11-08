450851fbabe973c13c258458ad57d82925d2fef0
rhrits <hritik.hritiksingh@gmail.com>
2024-11-08 17:29:31 +0530

    comment remove hero section

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,75 +1,3 @@
-// 'use client';
-
-
-// import { TypewriterEffect } from './ui/Typewriter-effect';
-
-
-
-// export default function HeroSection() {
-//   const words = [
-//     {
-//       text: "BUILD",
-//     },
-//     {
-//       text: "AWESOME",
-//     },
-    
-//     {
-//       text: "WITH",
-//     },
-    
-//     {
-//       text: "POLLUX",
-//       className: "text-blue-500 dark:text-blue-500",
-//     },
-//   ];
-
-
-//   const wordsA = [
-    
-    
-//     {
-//       text: "web",
-//       className: "text-green-500 dark:text-green-400",
-//     },
-//   ];
-
-
-//   return (
-//     <section className="h-screen flex flex-col items-center justify-center text-center space-y-6">
-
-// <TypewriterEffect words={words}/>
-//       {/* <h1 className="text-6xl font-extrabold text-white">
-
-
-
-
-//         <Typewriter text="POLLUXCHAIN" />
-//       </h1> */}
-//       <h2 className="text-2xl font-semibold text-[#fff]">
-//         Decentralize the
-         
-        
-//         <TypewriterEffect words={wordsA}/>
-        
-//       </h2>
-//       <div className="flex gap-4 mt-4">
-//       <button className="border-2 border-[#fff] px-6 py-3 rounded-md bg-[#8af969] text-black hover:border-[#ff] hover:shadow-glow transition-all">
-//   START
-// </button>
-
-
-
-
-//         <button className="border-2 border-white px-6 py-3 rounded-md hover:bg-white hover:text-black">
-//           Documentation
-//         </button>
-//       </div>
-//     </section>
-//   );
-// }
-// // 8af969
-
 
 
 'use client';
@@ -89,7 +17,7 @@ export default function HeroSection() {
     },
     {
       text: "POLLUX",
-      className: "text-blue-500 dark:text-blue-500",
+      className: "text-[#8af969]",
     },
   ];
 
@@ -113,7 +41,7 @@ export default function HeroSection() {
 
       {/* Buttons Section */}
       <div className="flex flex-col sm:flex-row gap-4 mt-6 justify-center items-center">
-        <button className="border-2 border-[#fff] px-6 py-3 rounded-md bg-[#8af969] text-black hover:border-[#ff] hover:shadow-glow transition-all w-full sm:w-auto">
+        <button className="border-2 border-[#fff] px-6 py-3 rounded-md bg-[#8af969] text-black hover:shadow-[0_0_10px_#8af969] transition-all w-full sm:w-auto">
           START
         </button>
         
@@ -124,3 +52,4 @@ export default function HeroSection() {
     </section>
   );
 }
+
